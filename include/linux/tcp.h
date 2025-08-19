1e4adfd8621077061c9f45fbbc3c5ac659b4b864
rainyxeon@gmail.com <rainyxeon@gmail.com>
2025-08-19 18:24:30 +0700

    BACKPORT: tcp fix full bit in header

--- include/linux/tcp.h
+++ include/linux/tcp.h
@@ -217,9 +217,9 @@ struct tcp_sock {
 		rate_app_limited:1,  /* rate_{delivered,interval_us} limited? */
 		fastopen_connect:1, /* FASTOPEN_CONNECT sockopt */
 		is_sack_reneg:1,    /* in recovery from loss with SACK reneg? */
-		unused:3;
+		tlp_orig_data_app_limited:1, /* app-limited before TLP rtx? */
+		unused:2;
 	u8	nonagle     : 4,/* Disable Nagle algorithm?             */
-		tlp_orig_data_app_limited:1; /* app-limited before TLP rtx? */
 		thin_lto    : 1,/* Use linear timeouts for thin streams */
 		unused1	    : 1,
 		repair      : 1,
