591fedfda4e00ac150b61a65d9bf1aed7cd6304a
rainyxeon@gmail.com <rainyxeon@gmail.com>
2025-08-19 17:53:55 +0700

    KernelSU: integrate scope-minimized manual hooks Thanks to backslashxx for this commit This refactors original KSU hooks to replace deep kernel function hooks with targeted hooks. This backports KernelSU pr#1657 and having pr#2084 elements (32-bit sucompat). It reduces the scope of kernel function interception and still maintains full fucntionality.
    This commit is a squash of the following:
    *	fs/exec: sys_execve: ksu_handle_execve_ksud/sucompat hook
    *	fs/exec: compat_sys_execve: ksu_handle_execve_sucompat hook
    	fs/open: sys_faccessat: ksu_handle_faccessat hook
    *	fs/read_write: sys_read: ksu_handle_sys_read hook
    *	fs/stat: sys_newfstatat: ksu_handle_stat hook
    *	fs/stat: sys_fstatat64: ksu_handle_stat hook
    *	drivers: input: input_event: ksu_handle_input_handle_event hook
    *	drivers: tty/pty.c: pts_unix98_lookup: ksu_handle_devpts hook
    
    references: KernelSU pr#1657, pr#2084
    	https://kernelsu.org/guide/how-to-integrate-for-non-gki.html
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- fs/stat.c
+++ fs/stat.c
@@ -147,9 +147,7 @@ int vfs_statx_fd(unsigned int fd, struct kstat *stat,
 	return error;
 }
 EXPORT_SYMBOL(vfs_statx_fd);
-#ifdef CONFIG_KSU
-extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
-#endif
+
 /**
  * vfs_statx - Get basic and extra attributes by filename
  * @dfd: A file descriptor representing the base dir for a relative filename
@@ -171,9 +169,7 @@ int vfs_statx(int dfd, const char __user *filename, int flags,
 	struct path path;
 	int error = -EINVAL;
 	unsigned int lookup_flags = LOOKUP_FOLLOW | LOOKUP_AUTOMOUNT;
-#ifdef CONFIG_KSU
-	ksu_handle_stat(&dfd, &filename, &flags);
-#endif
+
 	if ((flags & ~(AT_SYMLINK_NOFOLLOW | AT_NO_AUTOMOUNT |
 		       AT_EMPTY_PATH | KSTAT_QUERY_FLAGS)) != 0)
 		return -EINVAL;
@@ -356,6 +352,9 @@ SYSCALL_DEFINE2(newlstat, const char __user *, filename,
 
 	return cp_new_stat(&stat, statbuf);
 }
+#ifdef CONFIG_KSU
+ extern int ksu_handle_stat(int *dfd, const char __user **filename_user, int *flags);
+#endif
 
 #if !defined(__ARCH_WANT_STAT64) || defined(__ARCH_WANT_SYS_NEWFSTATAT)
 SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
@@ -363,7 +362,9 @@ SYSCALL_DEFINE4(newfstatat, int, dfd, const char __user *, filename,
 {
 	struct kstat stat;
 	int error;
-
+#ifdef CONFIG_KSU
+ 	ksu_handle_stat(&dfd, &filename, &flag);
+#endif
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
@@ -508,6 +509,9 @@ SYSCALL_DEFINE4(fstatat64, int, dfd, const char __user *, filename,
 	struct kstat stat;
 	int error;
 
+#ifdef CONFIG_KSU
+ 	ksu_handle_stat(&dfd, &filename, &flag); /* 32-bit su support */
+#endif
 	error = vfs_fstatat(dfd, filename, &stat, flag);
 	if (error)
 		return error;
