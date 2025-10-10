f210ec34287bde55892020e0496f9b84f114db0a
RainyXeon <rainyxeon@gmail.com>
2025-10-10 12:35:44 +0700

    FROMGIT: cmdline: fix syntax fail

--- fs/proc/cmdline.c
+++ fs/proc/cmdline.c
@@ -69,6 +69,7 @@ static void proc_command_line_init(void) {
     }
 #endif
 }
+#endif
 
 #ifdef CONFIG_KSU_SUSFS_SPOOF_CMDLINE_OR_BOOTCONFIG
 extern int susfs_spoof_cmdline_or_bootconfig(struct seq_file *m);
