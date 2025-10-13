e7ecf05afb1369666871b6eaba3d439fbfa52c60
RainyXeon <rainyxeon@gmail.com>
2025-10-13 18:11:47 +0700

    BACKPORT: fs: upstream susfs to 1.5.12@af29697b Patches: - KernelSU: fix try umounts cannot be umounted (1b1eefe9dd33c786ade55594559621c3ab774ba9...48ae4270469a8a7c7e0997a1f75d8ac74e881918) - kernel: Fixed fake mnt_id not assigned for umounted process (af29697b4d3098540a9912989dad5f41e71bb24a)
    Co-authored-by: simonpunk <simonpunk2016@gmail.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -2647,17 +2647,12 @@ static int do_loopback(struct path *path, const char *old_name,
 		unlock_mount_hash();
 	}
 #if defined(CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT) || defined(CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT)
-	// - We do not check anymore if boot-completed stage is triggered
-	//   just to stop the performance loss
 	// - Check if bind mounted path should be hidden and umounted automatically.
 	// And we target only process with ksu domain.
-	if (!susfs_is_boot_completed_triggered &&
-		(susfs_is_current_ksu_domain() || !strcmp(mnt->mnt_devname, "KSU")))
-	{
+	if (susfs_is_current_ksu_domain()) {
 #if defined(CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT)
-		if (susfs_is_auto_add_sus_bind_mount_enabled &&
-				susfs_auto_add_sus_bind_mount(old_name, &old_path)) {
-			goto orig_flow;
+		if (susfs_is_auto_add_sus_bind_mount_enabled) {
+			susfs_auto_add_sus_bind_mount(old_name, &old_path);
 		}
 #endif
 #if defined(CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT)
@@ -2666,9 +2661,6 @@ static int do_loopback(struct path *path, const char *old_name,
 		}
 #endif
 	}
-#if defined(CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT)
-orig_flow:
-#endif
 #endif // #if defined(CONFIG_KSU_SUSFS_AUTO_ADD_SUS_BIND_MOUNT) || defined(CONFIG_KSU_SUSFS_AUTO_ADD_TRY_UMOUNT_FOR_BIND_MOUNT)
 
 out2:
@@ -3977,6 +3969,10 @@ void susfs_reorder_mnt_id(void) {
 	get_mnt_ns(mnt_ns);
 	first_mnt_id = list_first_entry(&mnt_ns->list, struct mount, mnt_list)->mnt_id;
 	list_for_each_entry(mnt, &mnt_ns->list, mnt_list) {
+		// It is very important that we don't reorder the sus mount if it is not umounted
+		if (mnt->mnt_id == DEFAULT_KSU_MNT_ID) {
+			continue;
+		}
 		mnt->mnt.susfs_mnt_id_backup = mnt->mnt_id;
 		mnt->mnt_id = first_mnt_id++;
 	}
